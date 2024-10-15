f00f88d2d9839982cb5e7165ea42209a25edaac7
Asami <90178926+Asamijonsson@users.noreply.github.com>
2024-10-15 19:42:36 +0200

    added nav bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import { Header } from './components/Header'
-import './App.css'
+import { useState } from "react";
+import { Header } from "./components/Header";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header title={'hej'} />
+      <Header />
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,42 @@
-'use client';
+"use client";
 
-interface HeaderI {
-  title: string;
-}
+import { useState } from "react";
+import { motion } from "framer-motion";
+import style from "./Header.module.css";
+import { Nav } from "../Nav/Nav";
 
-export const Header = ({
-  title }: HeaderI) => {
+export const Header = () => {
+  const [navOpen, setnavOpen] = useState<boolean>(false);
+
+  const toggleHamburger = () => {
+    setnavOpen(!navOpen);
+  };
+
   return (
-    <>
-      <h1>{title}</h1>
-    </>
-  )
-}
+    <nav className={style.nav}>
+      <button className={style.toggleButton} onClick={toggleHamburger}>
+        <span className={style.bar}></span>
+        <span className={style.bar}></span>
+      </button>
+      {!navOpen ? (
+        <nav className={style.navbar}>
+          <Nav />{" "}
+        </nav>
+      ) : (
+        <motion.nav
+          key="content"
+          initial="collapsed"
+          animate="open"
+          exit="collapsed"
+          variants={{
+            open: { opacity: 1, height: "auto" },
+            collapsed: { opacity: 0, height: 0 },
+          }}
+          transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+        >
+          <Nav />
+        </motion.nav>
+      )}
+    </nav>
+  );
+};
